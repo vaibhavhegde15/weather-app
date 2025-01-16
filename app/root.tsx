5826605c3b445edc6f2b724d701bd012fdfdf3bb
vaibhavhegde15 <vaibhavhegde15@gmail.com>
2025-01-16 14:58:58 +0530

    Structure Changes

--- app/root.tsx
+++ app/root.tsx
@@ -1,39 +1,24 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 const theme = createTheme();
-import { LoaderFunction } from '@remix-run/node'
-import { requireUserId } from '~/utils/auth.server'
-
-// export const loader: LoaderFunction = async ({ request }) => {
-//   await requireUserId(request)
-//   return null
-// }
-export const links: LinksFunction = () => [
-
-];
+export const links: LinksFunction = () => [];
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <html lang="en">
-        <head>
-          <Meta />
-          <Links />
-        </head>
-        <body>
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
           <Outlet />
           <ScrollRestoration />
           <Scripts />
-        </body>
-      </html>
-    </ThemeProvider>
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
